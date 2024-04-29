0780b9cc890db17b92c19f348b6604b2bd9ed3d3
chelast55 <tim@amoto.de>
2024-04-29 20:27:02 +0200

    Split conf.py content and add additional Sphinx step (otions for post-processings of .rst files in between)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ project_author: str = "Avnu"
 project_release_version: str = "v4.0.2"
 project_year: str = "2024"
 
-html_theme: str = "alabaster" # "sphinx_rtd_theme"
+html_theme: str = "sphinx_rtd_theme"
 exhale_root_file_name: str = f"root_{project_name}"
 
 # Paths
@@ -31,12 +31,17 @@ graphviz_dot_path: Path = Path(r"C:\Program Files\Graphviz\bin\dot.exe")  # TODO
 INDEX_RST_CONTENT: str = f"""
 Welcome to {project_name}'s documentation!
 ============================{"="*len(project_name)}
-
+    
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+    :maxdepth: 2
+    :caption: Contents:
 
-   {str(exhale_containment_path).replace('\\', '/') + "/" + exhale_root_file_name}
+    {str(exhale_containment_path).replace('\\', '/') + "/" + exhale_root_file_name}
+
+Only to proof that graphs are included and until this is properly handled with exhale
+
+.. doxygenindex::
+    :allow-dot-graphs:
 
 Indices and tables
 ==================
@@ -66,6 +71,38 @@ doxygen_path: Path = Path(r"{str(doxygen_path)}")
 sphinx_path: Path = Path(r"{str(sphinx_path)}")
 exhale_path: Path = Path(r"{str(exhale_containment_path)}")
 graphviz_dot_path: Path = Path(r"{str(graphviz_dot_path)}")
+        
+graphviz_output_format = "svg"
+# graphviz_dot_args = ["-Gbgcolor=#FF00FF", "-Ncolor=#ffffff", "-Ecolor=#00ff00"]
+graphviz_dot = str(graphviz_dot_path)
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "{html_theme}"
+
+extensions = ["breathe", "sphinx.ext.graphviz", "sphinx.ext.autodoc", "sphinx.ext.autosummary", "myst_parser"]
+
+# -- Breathe configuration ---------------------------------------------------
+breathe_projects = {{
+    "{project_name}": str(doxygen_path / Path("xml"))
+}}
+breathe_default_project = "{project_name}"
+primary_domain = "cpp"
+highlight_language="cpp"
+
+"""
+
+CONF_PY_EXHALE_EXTENSION = f"""
+# -- Additional configuration ---------------------------------------------------
+
+extensions += ["exhale"]
 
 def specifications_for_kind(kind):
     \"\"\"
@@ -73,7 +110,7 @@ def specifications_for_kind(kind):
     for the associated Breathe directive.
     \"\"\"
     # Change the defaults for .. doxygenclass:: and .. doxygenstruct::
-    if kind == "class" or kind == "struct":
+    if kind == "class" or kind == "struct" or kind == "file":
         print("Doxygencalss or struct encountered and overriting")
         return [
             ":members:",
@@ -84,29 +121,8 @@ def specifications_for_kind(kind):
         ]
     # An empty list signals to Exhale to use the defaults
     else:
-        print("Something not class or struct encountered")
+        print("Something else encountered")
         return []
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = ["breathe", "exhale", "sphinx.ext.graphviz", "sphinx.ext.autodoc", "sphinx.ext.autosummary", "myst_parser"]
-
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = "{html_theme}"
-
-# -- Breathe configuration ---------------------------------------------------
-breathe_projects = {{
-    "{project_name}": str(doxygen_path / Path("xml"))
-}}
-breathe_default_project = "{project_name}"
-primary_domain = "cpp"
-highlight_language="cpp"
 
 # -- Exhale configuration ---------------------------------------------------
 exhale_args = {{
@@ -115,18 +131,54 @@ exhale_args = {{
     "rootFileName": "{exhale_root_file_name}.rst",
     "rootFileTitle": "{project_name} API",
     "doxygenStripFromPath": "..",
+    "fullToctreeMaxDepth": 1,
     # Suggested optional arguments
     "createTreeView": True,
     # TIP: if using the sphinx-bootstrap-theme, you need
-    # "treeViewIsBootstrap": True,
+    #"treeViewIsBootstrap": True,
     "exhaleExecutesDoxygen": True,
-    "exhaleDoxygenStdin": "INPUT = {project_path}",
+    "exhaleDoxygenStdin": \"\"\"
+        INPUT = {str(project_path).replace('\\', '\\\\')}
+        # OPTIMIZE_OUTPUT_FOR_C  = YES
+        EXTRACT_ALL = YES
+        EXTRACT_PRIVATE = YES
+        EXTRACT_PRIV_VIRTUAL   = YES
+        EXTRACT_PACKAGE        = YES
+        EXTRACT_STATIC         = YES
+        EXTRACT_LOCAL_METHODS  = YES
+        EXTRACT_ANON_NSPACES   = YES
+        SORT_MEMBERS_CTORS_1ST = YES
+        GENERATE_HTML = YES  # DEBUG
+        XML_PROGRAMLISTING = YES
+        XML_NS_MEMB_FILE_SCOPE = YES
+        GENERATE_AUTOGEN_DEF   = YES # EXPERIMENTAL
+        MACRO_EXPANSION        = YES
+        HIDE_UNDOC_RELATIONS   = YES
+        GENERATE_TREEVIEW = YES
+        HAVE_DOT = YES
+        CLASS_GRAPH = YES
+        COLLABORATION_GRAPH = YES
+        UML_LOOK = YES
+        UML_LIMIT_NUM_FIELDS   = 50
+        DOT_UML_DETAILS = YES
+        TEMPLATE_RELATIONS     = YES
+        INCLUDE_GRAPH = YES
+        INCLUDED_BY_GRAPH = YES
+        CALL_GRAPH = YES
+        CALLER_GRAPH = YES
+        GRAPHICAL_HIERARCHY = YES
+        DIRECTORY_GRAPH = YES
+        DOT_IMAGE_FORMAT = svg
+        INTERACTIVE_SVG = YES
+        DOT_PATH = {str(graphviz_dot_path).replace('\\', '\\\\') if not graphviz_dot_path is None else ""}
+        DOT_MULTI_TARGETS      = YES
+        # DOT_TRANSPARENT = YES
+    \"\"\",
+    "verboseBuild": True, # DEBUG
+    "generateBreatheFileDirectives": True, # DEBUG
     "customSpecificationsMapping": exhale_utils.makeCustomSpecificationsMapping(specifications_for_kind)
 }}
 
-graphviz_output_format = "svg"
-# graphviz_dot_args = ["-Gbgcolor=#FF00FF", "-Ncolor=#ffffff", "-Ecolor=#00ff00"]
-graphviz_dot = str(graphviz_dot_path)
 """
 
 if __name__ == "__main__":
@@ -137,16 +189,28 @@ if __name__ == "__main__":
     sphinx_path.mkdir(parents=True, exist_ok=True)
     exhale_containment_path_abs.mkdir(parents=True, exist_ok=True)
     if not graphviz_dot_path.exists(): raise OSError("dot.exe not found at given path")
+    chdir(sphinx_execution_main_path)
 
     # generate config files for sphinx+breathe+exhale+doxygen
-    with open(sphinx_execution_main_path / Path("index.rst"), "w+") as index_rst_file:
+    with open(Path("index.rst"), "w+") as index_rst_file:
         index_rst_file.write(INDEX_RST_CONTENT)
-    with open(sphinx_execution_main_path / Path("conf.py"), "w+") as conf_py_file:
-        conf_py_file.write(CONF_PY_CONTENT)
+    with open(Path("conf.py"), "w+") as conf_py_file:
+        conf_py_file.write(CONF_PY_CONTENT + CONF_PY_EXHALE_EXTENSION)
 
     # run sphinx+breath+exhale+doxygen
     print("\n--------------------")
     print("Generate sphinx...")
-    chdir(sphinx_execution_main_path)
     system(f"sphinx-build -b html . {sphinx_path}")
 
+    # exhale output post processings
+    # TODO: implement
+    #delete_directory_if_exists(exhale_containment_path)  # DEBUG
+    #delete_directory_if_exists(sphinx_path / Path("doc"))  # DEBUG
+    #system(f"breathe-apidoc -o {exhale_containment_path} -m {str(doxygen_path / Path("xml"))}")  # DEBUG
+
+    # adjust sphinx config for second run
+    with open(Path("conf.py"), "w+") as conf_py_file:
+        conf_py_file.write(CONF_PY_CONTENT)
+
+    # run sphinx again
+    system(f"sphinx-build -b html . {sphinx_path}")
