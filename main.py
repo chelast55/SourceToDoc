002f655bdeebd11d27575516626cdfe899391cf1
chelast55 <tim@amoto.de>
2024-07-01 15:12:37 +0200

    Next (large) wave of doxygen argument classification and implementation

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     doc_source_path: Path = doc_path / Path("src")
     doc_source_path_abs: Path = doc_path_abs / Path("src")
     doxygen_path: Path = Path()
+    warning_log_file_path: Path = doxygen_path / Path("doxygen_warnings.txt")
     if args.apidoc_toolchain == "doxygen-only":
         doxygen_path = doc_path
     elif args.apidoc_toolchain == "sphinx-based":
@@ -32,6 +33,10 @@ if __name__ == "__main__":
     exhale_include_path: Path = doc_path / exhale_containment_path
     graphviz_dot_path: Path = Path(r"C:\Program Files\Graphviz\bin\dot.exe")  # TODO: this needs to be addressed
     stylesheet_path: Path = doxygen_awesome_submodule_path / Path("doxygen-awesome.css")
+
+    # conditions
+    doxygen_xml_required: bool = not args.apidoc_toolchain == "doxygen"
+    doxygen_html_required: bool = args.apidoc_toolchain == "doxygen"
 
     # file contents
     DOXYFILE_CONTENT: str = f"""
@@ -100,16 +105,124 @@ if __name__ == "__main__":
         TIMESTAMP              = {args.timestamp}
         
         # Build related configuration options
+        EXTRACT_ALL             = YES
+        EXTRACT_PRIVATE         = {"YES" if args.disable_extract_private else "NO"}
+        EXTRACT_PRIV_VIRTUAL    = {"YES" if args.disable_extract_private_virtual else "NO"}
+        EXTRACT_PACKAGE         = {"YES" if args.disable_extract_package else "NO"}
+        EXTRACT_STATIC          = {"YES" if args.disable_extract_static else "NO"}
+        EXTRACT_LOCAL_CLASSES   = {"YES" if args.disable_extract_local_classes else "NO"}
+        EXTRACT_LOCAL_METHODS   = NO
+        EXTRACT_ANON_NSPACES    = {"YES" if args.disable_extract_anon_namespaces else "NO"}
+        RESOLVE_UNNAMED_PARAMS  = YES
+        HIDE_FRIEND_COMPOUNDS   = YES
+        HIDE_IN_BODY_DOCS       = NO
+        INTERNAL_DOCS           = YES
+        CASE_SENSE_NAMES        = SYSTEM
+        HIDE_SCOPE_NAMES        = {"YES" if args.hide_scope_names else "NO"}
+        HIDE_COMPOUND_REFERENCE = {"YES" if args.hide_compound_reference else "NO"}
+        SHOW_HEADERFILE         = {"YES" if args.disable_show_headerfile else "NO"}
+        SHOW_INCLUDE_FILES      = {"YES" if args.disable_show_include_files else "NO"}
+        SHOW_GROUPED_MEMB_INC   = {"YES" if args.disable_show_grounded_member_include else "NO"}
         
         # Configuration options related to warning and progress messages
+        QUIET                  = NO
+        WARNINGS               = {"YES" if args.disable_doxygen_warnings else "NO"}
+        WARN_IF_DOC_ERROR      = YES
+        WARN_IF_INCOMPLETE_DOC = YES
+        WARN_AS_ERROR          = NO
+        WARN_LOGFILE           = {str(warning_log_file_path).replace('\\', '\\\\')}
         
         # Configuration options related to the input files
+        INPUT                   = {str(project_path).replace('\\', '\\\\') if (args.input is None) else args.input}
+        INPUT_ENCODING          = {args.input_encoding}
+        INPUT_FILE_ENCODING     = {args.input_file_encoding}
+        FILE_PATTERNS           =    *.c \
+                                     *.cc \
+                                     *.cxx \
+                                     *.cxxm \
+                                     *.cpp \
+                                     *.cppm \
+                                     *.ccm \
+                                     *.c++ \
+                                     *.c++m \
+                                     *.java \
+                                     *.ii \
+                                     *.ixx \
+                                     *.ipp \
+                                     *.i++ \
+                                     *.inl \
+                                     *.idl \
+                                     *.ddl \
+                                     *.odl \
+                                     *.h \
+                                     *.hh \
+                                     *.hxx \
+                                     *.hpp \
+                                     *.h++ \
+                                     *.ixx \
+                                     *.l \
+                                     *.cs \
+                                     *.d \
+                                     *.php \
+                                     *.php4 \
+                                     *.php5 \
+                                     *.phtml \
+                                     *.inc \
+                                     *.m \
+                                     *.markdown \
+                                     *.md \
+                                     *.mm \
+                                     *.dox \
+                                     *.py \
+                                     *.pyw \
+                                     *.f90 \
+                                     *.f95 \
+                                     *.f03 \
+                                     *.f08 \
+                                     *.f18 \
+                                     *.f \
+                                     *.for \
+                                     *.vhd \
+                                     *.vhdl \
+                                     *.ucf \
+                                     *.qsf \
+                                     *.ice \
+                                     *.txt
+        RECURSIVE               = YES
+        EXCLUDE_SYMLINKS        = {"YES" if args.exclude_symlinks else "NO"}
+        #IMAGE_PATH             =
+        #INPUT_FILTER           =
+        #FILTER_PATTERNS        =
+        #FILTER_SOURCE_FILES    =
+        #FILTER_SOURCE_PATTERNS =
+        USE_MDFILE_AS_MAINPAGE  = TODO: locate readme
         
         # Configuration options related to source browsing
+        SOURCE_BROWSER          = {"YES" if args.disable_source_browser else "NO"}
+        INLINE_SOURCES          = {"YES" if args.inline_sources else "NO"}
+        STRIP_CODE_COMMENTS     = {"YES" if args.disable_strip_code_comments else "NO"}
+        REFERENCED_BY_RELATION  = {"YES" if args.disable_referenced_by_relation else "NO"}
+        REFERENCES_RELATION     = {"YES" if args.disable_references_relation else "NO"}
+        #REFERENCES_LINK_SOURCE  =
+        #SOURCE_TOOLTIPS         =
+        #USE_HTAGS               =
+        #VERBATIM_HEADERS        =
+        #CLANG_ASSISTED_PARSING  =
+        #CLANG_ADD_INC_PATHS     =
+        #CLANG_OPTIONS           =
+        #CLANG_DATABASE_PATH     =
         
         # Configuration options related to the alphabetical class index
+        ALPHABETICAL_INDEX = YES
         
         # Configuration options related to the HTML output
+        GENERATE_HTML         = {"YES" if doxygen_html_required else "NO"}
+        HTML_OUTPUT           = {"" if doxygen_html_required else "html"}
+        HTML_FILE_EXTENSION   = html
+        HTML_HEADER           = {str(args.html_header).replace('\\', '\\\\')}
+        HTML_FOOTER           = {str(args.html_footer).replace('\\', '\\\\')}
+        HTML_STYLESHEET       = {str(stylesheet_path).replace('\\', '\\\\')}
+        HTML_EXTRA_STYLESHEET = {str(args.html_extra_stylesheet).replace('\\', '\\\\')}
         
         # Configuration options related to the LaTeX output
         GENERATE_LATEX         = NO
@@ -119,31 +232,24 @@ if __name__ == "__main__":
         # Configuration options related to the man page output
         
         # Configuration options related to the XML output
+        GENERATE_XML            = {"YES" if (doxygen_xml_required or args.disable_generate_doxygen_xml) else "NO"}
+        XML_OUTPUT              = xml
+        XML_PROGRAMLISTING      = YES
+        XML_NS_MEMB_FILE_SCOPE  = YES
         
         # Configuration options related to the Perl module output
         
         # Configuration options related to the preprocessor
-        
-        # Configuration options related to external references
+        ENABLE_PREPROCESSING    = YES
+        MACRO_EXPANSION         = {"YES" if args.disable_macro_expansion else "NO"}
+        EXPAND_ONLY_PREDEF      = NO
+        SKIP_FUNCTION_MACROS    = {"YES" if args.disable_skip_function_macros else "NO"}
         
         # Configuration options related to diagram generator tools
 
-    
-        INPUT = {str(project_path).replace('\\', '\\\\')}
         # OPTIMIZE_OUTPUT_FOR_C  = YES
-        EXTRACT_ALL = YES
-        EXTRACT_PRIVATE = YES
-        EXTRACT_PRIV_VIRTUAL   = YES
-        EXTRACT_PACKAGE        = YES
-        EXTRACT_STATIC         = YES
-        EXTRACT_LOCAL_METHODS  = YES
-        EXTRACT_ANON_NSPACES   = YES
         SORT_MEMBERS_CTORS_1ST = YES
-        GENERATE_HTML = YES  # DEBUG
-        XML_PROGRAMLISTING = YES
-        XML_NS_MEMB_FILE_SCOPE = YES
         GENERATE_AUTOGEN_DEF   = YES # EXPERIMENTAL
-        MACRO_EXPANSION        = YES
         HIDE_UNDOC_RELATIONS   = YES
         GENERATE_TREEVIEW = YES
         HAVE_DOT = YES
@@ -163,10 +269,7 @@ if __name__ == "__main__":
         INTERACTIVE_SVG = YES
         DOT_PATH = {str(graphviz_dot_path).replace('\\', '\\\\') if not graphviz_dot_path is None else ""}
         DOT_MULTI_TARGETS      = YES
-        HTML_STYLESHEET = {str(stylesheet_path).replace('\\', '\\\\')}
         HTML_COLORSTYLE = LIGHT  # required with Doxygen >= 1.9.5
-        GENERATE_LATEX = NO
-        RECURSIVE = YES
     """
 
     INDEX_RST_CONTENT: str = f"""
