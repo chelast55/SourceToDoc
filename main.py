cc8bd88905431f987dd0d9b2d673feaa3f3f3ba7
chelast55 <tim@amoto.de>
2024-08-13 20:03:19 +0200

    Fix: Properly implement dg_dot_uml_details

--- main.py
+++ main.py
@@ -64,6 +64,16 @@ if __name__ == "__main__":
             readme_file_path = potential_readme_file
             break
 
+    # additional doxygen
+    match args.dg_dot_uml_details:
+        case "ALL":
+            dot_uml_details_translated: str = "YES"
+        case "SIMPLIFIED":
+            dot_uml_details_translated: str = "NO"
+        case "NONE":
+            dot_uml_details_translated: str = "NONE"
+
+
     # additional sphinx-specific
     sphinx_html_theme: str = "sphinx_rtd_theme"
     exhale_root_file_name: str = f"root_{args.project_name}"
@@ -73,7 +83,6 @@ if __name__ == "__main__":
         # Project related configuration options
         DOXYFILE_ENCODING      = UTF-8
         PROJECT_NAME           = {args.project_name}
-        PROJECT_AUTHOR         = {args.project_author}
         PROJECT_NUMBER         = {args.project_number}
         PROJECT_BRIEF          = {args.project_brief}
         PROJECT_LOGO           = {args.project_logo if (args.project_logo is not None and Path(args.project_logo).is_file()) else ""}
@@ -289,7 +298,7 @@ if __name__ == "__main__":
         GROUP_GRAPHS            = {"YES" if args.dg_disable_dot_graphs else "NO"}
         UML_LOOK                = {"YES" if args.dg_disable_uml_look else "NO"}
         UML_LIMIT_NUM_FIELDS    = {args.dg_uml_limit_num_fields if not args.dg_uml_limit_num_fields == 0 else 100}
-        DOT_UML_DETAILS         = {args.dg_dot_uml_details}
+        DOT_UML_DETAILS         = {dot_uml_details_translated}
         DOT_WRAP_THRESHOLD      = 20
         TEMPLATE_RELATIONS      = {"YES" if args.dg_disable_template_relations else "NO"}
         INCLUDE_GRAPH           = {"YES" if args.dg_disable_dot_graphs else "NO"}
@@ -441,6 +450,10 @@ if __name__ == "__main__":
     """
     #endregion file contents
 
+    # general stuff
+    if not project_path.exists():
+        raise OSError(f"No project at {project_path}. Path does not exist :/")
+
     # docstring preprocessing
     if args.converter is not None:
         print("\nComment Conversion:\n")
@@ -471,8 +484,6 @@ if __name__ == "__main__":
         if args.dg_html_theme == "doxygen-awesome" and not doxygen_stylesheet_path.is_file():
             parser.error("The stylesheet for doxygen-awesome was not found at its expected path. Try:\n$ git submodule update --init")
 
-
-        
         if args.apidoc_toolchain == "doxygen-only":
             # generate config file for Doxygen
             with open(Path("Doxyfile.in"), "w+") as doxyfile:
