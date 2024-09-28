887a37219f876155ab87e43b6651cab7fe8d16bc
chelast55 <tim@amoto.de>
2024-09-28 17:14:48 +0200

    Catch and Collect exceptions for each tool, to keep toolchain/runtime measurement running when individual tools fail

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ from sourcetodoc.uml.cli import run_uml_diagrams_generation
 
 
 if __name__ == "__main__":
+    error_in_cc: str = ""
+    error_in_dg: str = ""
+    error_in_tc: str = ""
+    error_in_lnk: str = ""
+    error_in_uml: str = ""
+
     t_start: float = time()
     parser: ConfiguredParser = ConfiguredParser()
     config = Config(parser.parse_args())
@@ -25,85 +31,118 @@ if __name__ == "__main__":
     # docstring preprocessing
     if config.args.converter is not None:
         print("\nComment Conversion:\n")
-        run_comment_converter(parser, config.project_path, **vars(config.args))
+        try:
+            run_comment_converter(parser, config.project_path, **vars(config.args))
+        except Exception as e:
+            error_in_cc = f"Exception occured while running the Comment Converter:\n{e}"
+            print(error_in_cc)
+            print("Continuing without further conversion...")
 
     t_converter: float = time()
 
     # documentation generation
     if config.args.disable_doc_gen:
         print("\nDocumentation Generation:\n")
-        run_documentation_generation(config)
+        try:
+            run_documentation_generation(config)
+        except Exception as e:
+            error_in_dg = f"Exception occured while running the Documentation Generation:\n{e}"
+            print(error_in_dg)
+            print("Continuing without generating further documentation...")
 
     t_docgen: float = time()
     
     # coverage
     if config.args.disable_test_cov:
         print("\nTest Coverage Evaluation:\n")
-        chdir(config.root_path)
-        if config.args.tc_coverage_type == "meson":
-            # TODO: allocate variables like project_path (the inline-if)
-            build_folder_name: Path = Path("build")
-            keep_build_folder: bool = False
-            meson_build_location: Path = config.project_path
-            meson_setup_args: list[str] = []
-            if config.args.tc_build_folder_name is not None:
-                build_folder_name = config.args.tc_build_folder_name
-            if config.args.tc_keep_build_folder is not None:
-                keep_build_folder = config.args.tc_keep_build_folder
-            if config.args.tc_meson_build_location is not None:
-                meson_build_location = Path(config.args.tc_meson_build_location)
-            if config.args.tc_meson_setup_args is not None:
-                if "--backend" in config.args.tc_meson_setup_args:
-                    raise Exception("Forbidden meson setup arg. --backend is not allowed. We only support ninja.")
-                meson_setup_args = config.args.tc_meson_setup_args.split(" ")
-            run_meson(config.testcoveragereport_path, meson_build_location, build_folder_name, keep_build_folder, meson_setup_args)
+        try:
+            chdir(config.root_path)
+            if config.args.tc_coverage_type == "meson":
+                # TODO: allocate variables like project_path (the inline-if)
+                build_folder_name: Path = Path("build")
+                keep_build_folder: bool = False
+                meson_build_location: Path = config.project_path
+                meson_setup_args: list[str] = []
+                if config.args.tc_build_folder_name is not None:
+                    build_folder_name = config.args.tc_build_folder_name
+                if config.args.tc_keep_build_folder is not None:
+                    keep_build_folder = config.args.tc_keep_build_folder
+                if config.args.tc_meson_build_location is not None:
+                    meson_build_location = Path(config.args.tc_meson_build_location)
+                if config.args.tc_meson_setup_args is not None:
+                    if "--backend" in config.args.tc_meson_setup_args:
+                        raise Exception("Forbidden meson setup arg. --backend is not allowed. We only support ninja.")
+                    meson_setup_args = config.args.tc_meson_setup_args.split(" ")
+                run_meson(config.testcoveragereport_path, meson_build_location, build_folder_name, keep_build_folder, meson_setup_args)
 
-        elif config.args.tc_coverage_type == "cmake":
-            build_folder_name: Path = Path("build")
-            keep_build_folder: bool = False
-            project_root: Path = config.project_path
-            cmake_configure_args: list[str] = [".."]
-            cmake_build_args: list[str] = ["."]
-            ctest_args: list[str] = []
-            ctest_substitute: list[str] = []
+            elif config.args.tc_coverage_type == "cmake":
+                build_folder_name: Path = Path("build")
+                keep_build_folder: bool = False
+                project_root: Path = config.project_path
+                cmake_configure_args: list[str] = [".."]
+                cmake_build_args: list[str] = ["."]
+                ctest_args: list[str] = []
+                ctest_substitute: list[str] = []
 
-            if config.args.tc_build_folder_name is not None:
-                build_folder_name = config.args.tc_build_folder_name
-            if config.args.tc_keep_build_folder is not None:
-                keep_build_folder = config.args.tc_keep_build_folder
-            if config.args.tc_cmake_configure_args is not None:
-                cmake_configure_args = config.args.tc_cmake_configure_args.split(" ")
-            if config.args.tc_cmake_build_args is not None:
-                cmake_build_args = config.args.tc_cmake_build_args.split(" ")
-            if config.args.tc_ctest_args is not None:
-                ctest_args = config.args.tc_ctest_args.split(" ")
-            if config.args.tc_ctest_substitute is not None:
-                ctest_substitute = config.args.tc_ctest_substitute.split(" ")
+                if config.args.tc_build_folder_name is not None:
+                    build_folder_name = config.args.tc_build_folder_name
+                if config.args.tc_keep_build_folder is not None:
+                    keep_build_folder = config.args.tc_keep_build_folder
+                if config.args.tc_cmake_configure_args is not None:
+                    cmake_configure_args = config.args.tc_cmake_configure_args.split(" ")
+                if config.args.tc_cmake_build_args is not None:
+                    cmake_build_args = config.args.tc_cmake_build_args.split(" ")
+                if config.args.tc_ctest_args is not None:
+                    ctest_args = config.args.tc_ctest_args.split(" ")
+                if config.args.tc_ctest_substitute is not None:
+                    ctest_substitute = config.args.tc_ctest_substitute.split(" ")
 
-            run_cmake(config.testcoveragereport_path, project_root, cmake_configure_args, cmake_build_args, ctest_args, ctest_substitute, build_folder_name, keep_build_folder)
+                run_cmake(config.testcoveragereport_path, project_root, cmake_configure_args, cmake_build_args, ctest_args, ctest_substitute, build_folder_name, keep_build_folder)
 
-        elif config.args.tc_coverage_type == "generic":
-            if config.args.tc_generic_report_location is not None:
-                report_folder: Path = Path(config.args.tc_generic_report_location)
-                if report_folder.exists() and report_folder.is_dir():  # if build failed or is impossible, there is nothing to copy
-                    copytree(report_folder, config.testcoveragereport_path, dirs_exist_ok=True)
+            elif config.args.tc_coverage_type == "generic":
+                if config.args.tc_generic_report_location is not None:
+                    report_folder: Path = Path(config.args.tc_generic_report_location)
+                    if report_folder.exists() and report_folder.is_dir():  # if build failed or is impossible, there is nothing to copy
+                        copytree(report_folder, config.testcoveragereport_path, dirs_exist_ok=True)
+                    else:
+                        raise Exception(f"{config.args.tc_generic_report_location} is not a folder. Please pass the testcoverage containing folder.")
                 else:
-                    raise Exception(f"{config.args.tc_generic_report_location} is not a folder. Please pass the testcoverage containing folder.")
-            else:
-                raise Exception("Generic test coverage type was selected but no config.args.tc_generic_report_location was passed.")
+                    raise Exception("Generic test coverage type was selected but no config.args.tc_generic_report_location was passed.")
+        except Exception as e:
+            error_in_tc = f"Exception occured while running the Test Coverage Evaluation:\n{e}"
+            print(error_in_tc)
+            print("Continuing without generating test coverage report...")
 
-        # Link coverage report and documentation
-        if config.args.disable_doc_gen:  # can not link TC report to generated documentation, if no documentation was generated
-            link_tc_report_and_documentation_main(config.out_path_relative / Path(config.args.project_name))
-            link_all_tc_report_and_documentation_files(config.out_path_relative / Path(config.args.project_name))
+            # Link coverage report and documentation
+            if config.args.disable_doc_gen:  # can not link TC report to generated documentation, if no documentation was generated
+                try:
+                    link_tc_report_and_documentation_main(config.out_path_relative / Path(config.args.project_name))
+                    link_all_tc_report_and_documentation_files(config.out_path_relative / Path(config.args.project_name))
+                except Exception as e:
+                    error_in_lnk = f"Exception occured while linking API Documentation and Test Coverage Report:\n{e}"
+                    print(error_in_lnk)
+                    print("Continuing without linking...")
 
     t_coverage: float = time()
 
     # uml diagrams
     if config.args.generate_uml_diagrams:
-        run_uml_diagrams_generation(parser, config)
+        try:
+            run_uml_diagrams_generation(parser, config)
+        except Exception as e:
+            error_in_lnk = f"Exception occured while generating UML diagrams:\n{e}"
+            print(error_in_uml)
+            print("Continuing without generating further UML...")
     
     t_uml_diagrams: float = time()
+
+    # print if errors occured in any of the tools
+    print("\n")
+    print(error_in_cc)
+    print(error_in_dg)
+    print(error_in_tc)
+    print(error_in_lnk)
+    print(error_in_uml)
 
     if config.args.measure_runtime:
         print("\n")
