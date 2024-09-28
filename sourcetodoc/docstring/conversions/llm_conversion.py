2f1e3206bb202aea2f7db6b37b7f3b9c13a19e6c
kevin <k-n@hutvoid.de>
2024-09-28 21:18:16 +0200

    Use global config and make LLM prompts user configurable

--- sourcetodoc/docstring/conversions/llm_conversion.py
+++ sourcetodoc/docstring/conversions/llm_conversion.py
@@ -19,10 +19,16 @@ class LLMConversion(Conversion[CType | CXXType]):
         CXXType.METHOD,
     }
 
-    default_c_system_prompt = "You are a coder that converts comments on c functions to doxygen style comments that start with \"/**\" without changing the text."
-    default_cxx_system_prompt = "You are a coder that converts comments on C++ functions to doxygen style comments that start with \"/**\" without changing the text."
+    DEFAULT_C_SYSTEM_PROMPT = "You are a coder that converts comments on C functions to doxygen style comments that start with \"/**\" without changing the text."
+    DEFAULT_CXX_SYSTEM_PROMPT = "You are a coder that converts comments on C++ functions to doxygen style comments that start with \"/**\" without changing the text."
 
-    def __init__(self, llm: LLM) -> None:
+    def __init__(
+            self, llm: LLM,
+            c_system_prompt: str = DEFAULT_C_SYSTEM_PROMPT,
+            c_user_prompt_template: str = "{}",
+            cxx_system_prompt: str = DEFAULT_CXX_SYSTEM_PROMPT,
+            cxx_user_prompt_template: str = "{}",
+        ) -> None:
         """
         Creates a new object.
 
@@ -32,6 +38,10 @@ class LLMConversion(Conversion[CType | CXXType]):
             The LLM.
         """
         self.llm_helper = LLMConversionHelper[CType | CXXType](llm)
+        self.c_system_prompt = c_system_prompt
+        self.c_user_prompt_template = c_user_prompt_template
+        self.cxx_system_prompt = cxx_system_prompt
+        self.cxx_user_prompt_template = cxx_user_prompt_template
 
     @override
     def calc_conversion(self, comment: Comment[CType | CXXType]) -> ConvResult:
@@ -54,14 +64,16 @@ class LLMConversion(Conversion[CType | CXXType]):
         if comment.symbol_type is CType.FUNCTION:
             return self.llm_helper.calc_conversion_with_llm(
                 comment,
-                self.__class__.default_c_system_prompt,
-                CommentStyle.JAVADOC_BLOCK
+                self.c_system_prompt,
+                CommentStyle.JAVADOC_BLOCK,
+                self.c_user_prompt_template
             )
         elif comment.symbol_type in self.__class__._CXX_INCLUDE_TYPES:
             return self.llm_helper.calc_conversion_with_llm(
                 comment,
-                self.__class__.default_cxx_system_prompt,
-                CommentStyle.JAVADOC_BLOCK
+                self.cxx_system_prompt,
+                CommentStyle.JAVADOC_BLOCK,
+                self.cxx_user_prompt_template
             )
         else:
             return ConvUnsupported("Comment is not attached to a C function")
--- sourcetodoc/docstring/conversions/llm_conversion_helper.py
+++ sourcetodoc/docstring/conversions/llm_conversion_helper.py
@@ -1,5 +1,4 @@
 import re
-from typing import Optional
 
 from ..command_style import CommandStyle
 from ..comment_style import CommentStyle, CommentStyler
@@ -33,28 +32,32 @@ class LLMConversionHelper[T]:
             self,
             comment: Comment[T],
             system_prompt: str,
-            output_style: CommentStyle
+            output_style: CommentStyle,
+            user_prompt_template: str = "{}"
         ) -> ConvResult:
         """
         Calculates new comment texts results with a LLM.
 
         Steps:
         1. `comment_text` in `comment` will be parsed and formatted by `CommandStyler`.
-        2. Pass the formatted comment to the LLM.
-        3. Extract the first found `/*...*/` part in the output of the LLM.
-        4. Replace `@command with` `\\command` in the new comment.
-        5. Prepend `AI_GENERATED` to the new comment.
-        6. Format the new comment.
+        2. Insert the formatted comment in {} of `prompt`.
+        3. Pass `system_prompt` with `prompt` to the LLM.
+        4. Extract the first found `/*...*/` part in the output of the LLM.
+        5. Replace `@command with` `\\command` in the new comment.
+        6. Prepend `AI_GENERATED` to the new comment.
+        7. Format the new comment.
 
         Parameters
         ----------
-        comment : Comment[T]
+        comment: Comment[T]
             The comment that is passed to the LLM as the prompt.
-        system_prompt : str
+        system_prompt: str
             The system prompt that is passed to the LLM.
-        output_style : CommentStyle
+        output_style: CommentStyle
             The comment style of the output.
-        
+        prompt: str
+            The prompt that is passed to the LLM.
+
         Returns
         -------
         ConversionResult[T]
@@ -62,7 +65,6 @@ class LLMConversionHelper[T]:
             A ConvEmpty object if `comment` is already a Doxygen style comment.
             A ConvError object if `comment` cannot be parsed in Step 1,
             or if no `/*...*/` is found the output of the LLM.
-
         """
         # Format the comment text and append the symbol text
         match CommentStyler.parse_comment(comment.comment_text):
@@ -74,10 +76,11 @@ class LLMConversionHelper[T]:
             case CommentStyler() as input_styler:
                 comment_formatted = input_styler.construct_comment()
 
-        prompt = comment_formatted + "\n" + comment.symbol_text
+        prompt_part = comment_formatted + "\n" + comment.symbol_text
+        user_prompt = user_prompt_template.format(prompt_part)
 
         # Call LLM
-        llm_output = self.llm.call_llm(system_prompt, prompt)
+        llm_output = self.llm.call_llm(system_prompt, user_prompt)
 
         # Extract the comment part from the output
         new_comment = self._extract_comment(llm_output)
@@ -101,7 +104,7 @@ class LLMConversionHelper[T]:
 
 
     @classmethod
-    def _extract_comment(cls, result: str) -> Optional[str]:
+    def _extract_comment(cls, result: str) -> str | None:
         match = cls._LINE_COMMENT_PATTERN.search(result)
         if match is None:
             match = cls._BLOCK_COMMENT_PATTERN.search(result)
